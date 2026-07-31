c751656cbf4963e54508a90e367e89a0b468ddbd
Anusha-R-Sharath <anushar@nmamit.in>
2026-07-31 15:40:17 +0530

    Final_Version Edge AI on STM32 from NanoEdge_AI

--- STM32/NanoEdgeAI/Debug/Core/Src/main.d
+++ STM32/NanoEdgeAI/Debug/Core/Src/main.d
@@ -30,7 +30,8 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_pwr_ex.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart.h \
  ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal_uart_ex.h \
- ../Core/Inc/imu.h ../Core/Inc/main.h ../Core/Inc/lsm6dsl_reg.h
+ ../Core/Inc/imu.h ../Core/Inc/main.h ../Core/Inc/lsm6dsl_reg.h \
+ D:/Anusha_TinyML_FDP/TinyML_Workshop_Nitte-/STM32/NanoEdgeAI/NEAI/Inc/NanoEdgeAI.h
 ../Core/Inc/main.h:
 ../Drivers/STM32L4xx_HAL_Driver/Inc/stm32l4xx_hal.h:
 ../Core/Inc/stm32l4xx_hal_conf.h:
@@ -66,3 +67,4 @@ Core/Src/main.o: ../Core/Src/main.c ../Core/Inc/main.h \
 ../Core/Inc/imu.h:
 ../Core/Inc/main.h:
 ../Core/Inc/lsm6dsl_reg.h:
+D:/Anusha_TinyML_FDP/TinyML_Workshop_Nitte-/STM32/NanoEdgeAI/NEAI/Inc/NanoEdgeAI.h:
